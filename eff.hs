0dd798261150e7e1bcb2b48e6b6b8b2a3c405215
philopon <philopon.dependence@gmail.com>
2014-09-28 23:42:33 +0900

    add scale, IO to eff.

--- eff.hs
+++ eff.hs
@@ -10,6 +10,7 @@ import qualified Control.Monad.State.Strict  as M
 import qualified Control.Monad.Writer.Strict as M
 
 import qualified Control.Eff as EE
+import qualified Control.Eff.Lift as EE
 import qualified Control.Eff.State.Strict  as EE
 import qualified Control.Eff.Reader.Strict as EE
 import qualified Control.Eff.Writer.Strict as EE
@@ -32,6 +33,9 @@ monad = do
 runMonad :: Int -> Int
 runMonad i = getSum . snd . M.runWriter $ M.runStateT (M.runReaderT (replicateM_ i monad) (1 :: Int)) (1 :: Int)
 
+runMonadIO :: Int -> IO Int
+runMonadIO i = fmap (getSum . snd) . M.runWriterT $ M.runStateT (M.runReaderT (replicateM_ i monad) (1 :: Int)) (1 :: Int)
+
 extensibleEffects :: (EE.Member (EE.Reader Int) r, EE.Member (EE.State Int) r, EE.Member (EE.Writer (Sum Int)) r) => EE.Eff r ()
 extensibleEffects = do
     (d :: Int) <- EE.ask
@@ -41,6 +45,10 @@ extensibleEffects = do
 
 runEE :: Int -> Int
 runEE i = getSum . fst . EE.run $ EE.runMonoidWriter $
+    EE.runState (1 :: Int) (EE.runReader (replicateM_ i extensibleEffects) (1 :: Int))
+
+runEEIO :: Int -> IO Int
+runEEIO i = fmap (getSum . fst) . EE.runLift $ EE.runMonoidWriter $
     EE.runState (1 :: Int) (EE.runReader (replicateM_ i extensibleEffects) (1 :: Int))
 
 effin :: (Effin.EffectWriter (Sum Int) l, Effin.EffectReader Int l, Effin.EffectState Int l) => Effin.Effect l ()
@@ -54,9 +62,44 @@ runEffin :: Int -> Int
 runEffin i = getSum . snd . Effin.runEffect . Effin.runWriter $
     Effin.runState (1 :: Int) (Effin.runReader (1 :: Int) (replicateM_ i effin))
 
+runEffinIO :: Int -> IO Int
+runEffinIO i = fmap (getSum . snd) . Effin.runLift . Effin.runWriter $
+    Effin.runState (1 :: Int) (Effin.runReader (1 :: Int) (replicateM_ i effin))
+
+m :: Int
+m = 1000
+
+n :: Int
+n = 10000
+
 main :: IO ()
 main = defaultMain
-    [ bench "monad"              $ nf runMonad 100000
-    , bench "extensible-effects" $ nf runEE    100000
-    , bench "effin"              $ nf runEffin 100000
+    [ bgroup "pure" 
+        [ bgroup "monad"
+            [ bench (show m) $ nf runMonad m
+            , bench (show n) $ nf runMonad n
+            ]
+        , bgroup "extensible-effects"
+            [ bench (show m) $ nf runEE m
+            , bench (show n) $ nf runEE n
+            ]
+        , bgroup "effin"
+            [ bench (show m) $ nf runEffin m
+            , bench (show n) $ nf runEffin n
+            ]
+        ]
+    , bgroup "IO"
+        [ bgroup "monad"
+            [ bench (show m) $ nfIO $ runMonadIO m
+            , bench (show n) $ nfIO $ runMonadIO n
+            ]
+        , bgroup "extensible-effects"
+            [ bench (show m) $ nfIO $ runEEIO m
+            , bench (show n) $ nfIO $ runEEIO n
+            ]
+        , bgroup "effin"
+            [ bench (show m) $ nfIO $ runEffinIO m
+            , bench (show n) $ nfIO $ runEffinIO n
+            ]
+        ]
     ]
